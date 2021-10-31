1fe5b401b3206c063abb0889848b7f417c7fb377
NAASAfiqah <90669521+NAASAfiqah@users.noreply.github.com>
2021-10-31 11:40:53 +0800

    Add files via upload

---
+++ dataexplorationHeartDisease.sql
@@ -0,0 +1,29 @@
+Select*
+From PortfolioProjectHeartDisease..HeartDisease
+Order by Age
+
+Select Age, Sex
+From PortfolioProjectHeartDisease..HeartDisease
+Order by Age
+
+
+--Let's look at maxHR and cholesterol numbers
+Select Age, Cholesterol, MaxHR
+From PortfolioProjectHeartDisease..HeartDisease
+Order by Age
+
+--Let's look at ATA, TA, NAP, ASV
+Select Age, ChestPainType
+From PortfolioProjectHeartDisease..HeartDisease
+Order by Age
+
+
+Select Age,Sex, Cholesterol,MaxHR
+From PortfolioProjectHeartDisease..HeartDisease
+Order by Age
+
+--Looking at age vs chest pain type
+Select Age,ChestPainType
+From PortfolioProjectHeartDisease..HeartDisease
+Order by Age
+
